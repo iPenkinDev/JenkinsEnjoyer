06544cf6b32241a656755ea5bcf400514e277fdd
Igor Penkin <igor.penkin.dev@gmail.com>
2023-06-02 22:50:04 +0300

    initial commit

---
+++ src/main/java/com/example/jenkinsenjoyer/JenkinsEnjoyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsenjoyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsEnjoyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsEnjoyerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsenjoyer/JenkinsEnjoyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsenjoyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsEnjoyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
