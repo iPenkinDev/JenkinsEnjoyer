559d65b1c0ed23f85805857c4a5480fa3944a880
Igor Penkin <igor.penkin.dev@gmail.com>
2023-06-04 00:18:30 +0300

    -

--- src/main/java/com/example/jenkinsenjoyer/controller/HelloWorldController.java
+++ src/main/java/com/example/jenkinsenjoyer/controller/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
     @GetMapping("/index")
     @ResponseBody
     public String helloWorld() {
-        return "Hello world #13\nPobeda\nУрааа";
+        return "Hello world #14\nPobeda\nУрааа";
     }
 }
