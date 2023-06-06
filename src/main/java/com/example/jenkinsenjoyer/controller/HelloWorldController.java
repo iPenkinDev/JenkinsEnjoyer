baef2253b9b6e98205d91cac68557af4198dd734
Igor Penkin <igor.penkin.dev@gmail.com>
2023-06-06 10:24:08 +0300

    initial com

--- src/main/java/com/example/jenkinsenjoyer/controller/HelloWorldController.java
+++ src/main/java/com/example/jenkinsenjoyer/controller/HelloWorldController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
-
 @Controller
 public class HelloWorldController {
 
     @GetMapping("/index")
     @ResponseBody
     public String helloWorld() {
-        return "Hello World!!";
+        return "Hello Мир!!!";
     }
 }
